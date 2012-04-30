34c177b931060dfce999002f4d95c89d92df8dcc
nanoo@nanoo-laptop <nanoo@meta.ua>
2012-04-30 10:28:02 +0200

    * initial commit

---
+++ java/arraylist/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.lang.Integer;
+
+public class Main {
+	public static void main(String[] args) {
+		ArrayList <Integer> l = new ArrayList <Integer>();
+		l.add(1);
+		l.add(2);
+		l.add(3);
+		for (Integer i: l) {
+			System.out.println(i);
+		}
+	}
+}
